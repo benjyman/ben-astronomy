6e57d3ff7f3ae37091354284a95e9009ccb6068e
Benjamin McKinley <bmckinley@galaxy-2.pawsey.org.au>
2017-05-05 11:27:33 +0800

    crop imagae test

--- moon/crop_images.py
+++ moon/crop_images.py
@@ -16,8 +16,8 @@ band_centre_chans=[69,93,121,145,169]
 for centre_chan in band_centre_chans:
    #read the info files to find out how many observations there are and what the on and off-moon obsids are:
    if (galaxy):
-     on_moon_filename="20150926_moon1/%s/20150926_moon_%s.txt" % (str(centre_chan),str(centre_chan))
-     off_moon_filename="20150929_off_moon1/%s/20150929_off_moon1_%s.txt" % (str(centre_chan),str(centre_chan))   
+     on_moon_filename="20150926_moon1/%s/20150926_moon_%s_test.txt" % (str(centre_chan),str(centre_chan))
+     off_moon_filename="20150929_off_moon1/%s/20150929_off_moon1_%s_test.txt" % (str(centre_chan),str(centre_chan))   
    else:
      on_moon_filename="20150926_moon_%s_test.txt" % (str(centre_chan))
      off_moon_filename="20150929_off_moon1_%s_test.txt" % (str(centre_chan))
