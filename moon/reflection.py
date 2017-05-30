870186bc0cc4d120d627845f300f1b4d1155053b
Ben Mckinley <benjyman1024@gmail.com>
2017-05-30 16:44:42 +1000

    additions to reflection.py

--- moon/reflection.py
+++ moon/reflection.py
@@ -2,64 +2,120 @@
 #algorithm from Appendix B Vedantham et al 2015
 #first obsid is 11273217455 '2015/09/26 16:55:28'
 
-from ephem import *
+#from ephem import *
 from time import *
 from astropy import units as u
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, Distance
+from skyfield.api import Topos, load
+import numpy as np
+
+moon_radius_km=1738.1
+
+r2d = 180.0/np.pi
+d2r = np.pi/180.0
+
+r2h = 12.0/np.pi
+h2r = np.pi/12.0
 
 
-r2d = 180.0/pi
-d2r = pi/180.0
-
-r2h = 12.0/pi
-h2r = pi/12.0
-
-
-#time
-obs_date='2015/09/26 16:55:28'
-obs_epoch='2000/1/1 12'
+#set time
+#ephem
+##obs_date='2015/09/26 16:55:28'
+##obs_epoch='2000/1/1 12'
+#skyfield:
+ts = load.timescale()
+t=ts.utc(2015, 9, 26, 16, 55, 28.0)
 
 
-# Create an observer
-MRO = Observer()
+##With Pyephem:
+## Create an observer
+##MRO = Observer()
 
-# Set the observer at the MWA
-MRO.lat, MRO.long, MRO.elevation = '-26:42:11.95', '116:40:14.93', 377.83
+## Set the observer at the MWA
+##MRO.lat, MRO.long, MRO.elevation = '-26:42:11.95', '116:40:14.93', 377.83
 
-# Set the time.
-if obs_date  != '': MRO.date  = obs_date
-if obs_epoch != '': MRO.epoch = obs_epoch
+## Set the time.
+##if obs_date  != '': MRO.date  = obs_date
+##if obs_epoch != '': MRO.epoch = obs_epoch
+
+planets = load('de421.bsp')
+
+earth=planets['earth']
+
+MWA = earth + Topos(latitude_degrees=-26.70331940, longitude_degrees=116.67081524 ,elevation_m=377.83)
+moon = planets['moon']
+astrometric_moon_centre_ephem=MWA.at(t).observe(moon)
+moon_centre_ra, moon_centre_dec, moon_centre_distance = astrometric_moon_centre_ephem.radec()
+
+#print(moon_centre_ra.hstr())
+#print(moon_centre_dec.dstr())
+#print(moon_centre_distance.km)
+
+dec_offset=0.0*u.degree
+ra_offset=0.0*u.degree
+
+moon_pix_dec = moon_centre_dec.to(u.deg)+dec_offset
+moon_pix_ra=moon_centre_ra.to(u.deg)+ra_offset
+moon_pix_distance_km=moon_centre_distance.km - moon_radius_km
 
 
-#RA Dec of the Moon
-moon = Moon(MRO);
-az = moon.az  * r2d
-el = moon.alt * r2d
-ra0, dec0 = MRO.radec_of(moon.az, moon.alt)
-ra_deg=r2d*(hours(ra0))
-dec_deg=r2d*(degrees(dec0))
 
-print "Moon is at RA %s degrees Dec %s " % (ra_deg,dec_deg)
-print "Alt %s, Az %s" % (el,az)
+moon_centre_skycoord=SkyCoord(ra=moon_centre_ra.to(u.deg), dec=moon_centre_dec.to(u.deg), distance=moon_centre_distance.km*u.km)
+#print float(moon_centre_skycoord.cartesian.x/u.km)
+
+moon_centre_vector=np.array([moon_centre_skycoord.cartesian.x/u.km,moon_centre_skycoord.cartesian.y/u.km,moon_centre_skycoord.cartesian.z/u.km])
+print moon_centre_vector
+
+moon_pixel_skycoord=SkyCoord(ra=moon_pix_ra, dec=moon_pix_dec, distance=moon_pix_distance_km*u.km)
+moon_pixel_vector=np.array([moon_pixel_skycoord.cartesian.x/u.km,moon_pixel_skycoord.cartesian.y/u.km,moon_pixel_skycoord.cartesian.z/u.km])
+print moon_pixel_vector
+
+normal_vector=moon_centre_vector-moon_pixel_vector
+print normal_vector
+
+##RA Dec of the Moon centre
+##moon_centre = Moon(MRO);
+##moon_centre_az = moon_centre.az  * r2d
+##moon_centre_el = moon_centre.alt * r2d
+##moon_centre_ra0, moon_centre_dec0 = MRO.radec_of(moon_centre.az, moon_centre.alt)
+##moon_centre_ra_deg=r2d*(hours(moon_centre_ra0))
+##moon_centre_dec_deg=r2d*(degrees(moon_centre_dec0))
+
+##print "Moon centre is at RA %s degrees Dec %s " % (moon_centre_ra_deg,moon_centre_dec_deg)
+##print "Alt %s, Az %s" % (moon_centre_el,moon_centre_az)
 
 #define HEALPIX (N=32) grid for Lunar surface
 
 #extract the pixels visible to the MWA
 
-#For each pixel:
-#calculate position vector i 
-pixel_position_ra_dec=SkyCoord(ra_deg*u.degree, dec_deg*u.degree,distance=1*u.m)
-print pixel_position_ra_dec
-pixel_position_cartesian_x=pixel_position_ra_dec.cartesian.x
-print pixel_position_cartesian_x
-pixel_position_cartesian_y=pixel_position_ra_dec.cartesian.y
-print pixel_position_cartesian_y
-pixel_position_cartesian_z=pixel_position_ra_dec.cartesian.z
-print pixel_position_cartesian_z
 
-#make the vector
 
-#do the same of rthe MWA position with EarthLocation
+#For Moon centre
+
+##calculate position vector i 
+##moon_centre_pixel_position_ra_dec=SkyCoord(moon_centre_ra_deg*u.degree, moon_centre_dec_deg*u.degree,distance=1*u.m)
+##print moon_centre_pixel_position_ra_dec
+##moon_centre_pixel_position_cartesian_x=moon_centre_pixel_position_ra_dec.cartesian.x
+##print moon_centre_pixel_position_cartesian_x
+##moon_centre_pixel_position_cartesian_y=moon_centre_pixel_position_ra_dec.cartesian.y
+##print moon_centre_pixel_position_cartesian_y
+##moon_centre_pixel_position_cartesian_z=moon_centre_pixel_position_ra_dec.cartesian.z
+##print moon_centre_pixel_position_cartesian_z
+
+##define the moon centre position vector
+##moon_centre_pixel_position_vector=[moon_centre_pixel_position_cartesian_x,moon_centre_pixel_position_cartesian_y,moon_centre_pixel_position_cartesian_z]
+
+
+#then for each Moon pixel
+
+#Get RA Dec from healpix function
+#define position vector as above (for now use centre)
+##pixel_position_vector=moon_centre_pixel_position_vector
+
+#calculate vector normal to Moon surface 
+#normal_vector=
+
+#do the same of rthe MWA position with EarthLocation.from_geodetic(lon, lat, height=0.0, ellipsoid=None)
 
 
 #calculate normal vector n
