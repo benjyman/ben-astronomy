1bcb6f17379207b6ee7303803b218eb8522c9171
Benjamin McKinley <bmckinley@galaxy-2.pawsey.org.au>
2017-06-28 12:58:04 +0800

    169 on moon defaults

---
+++ moon/default_scripts/169/on_moon/4a_default_image_ms_cotter_peeled_moon_169_chans_dirty.py
@@ -0,0 +1,1 @@
+python /group/mwaeor/CODE/MWA_Tools/scripts/generate_mwac_qimage_concat_ms.py --cotter --track_moon --ionpeeled --no_pbcorr --tagname=20150926_moon_169  --imsize=3328 --pol="xx,xy,yx,yy"  --wsclean_options=" -niter 0 -datacolumn CORRECTED_DATA  -scale 0.0085 -weight natural  -smallinversion  -channelsout 24 -make-psf  " 20150926_moon_169.txt  20150926_moon_169
