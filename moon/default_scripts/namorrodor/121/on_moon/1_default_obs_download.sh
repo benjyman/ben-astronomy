3100a4c74ea7af2939e9e5ee46db559852197d48
benjamin <benjamin@namorrodor>
2017-07-31 16:10:07 +0800

    changes to defaults

---
+++ moon/default_scripts/namorrodor/121/on_moon/1_default_obs_download.sh
@@ -0,0 +1,1 @@
+python /data/code/git/ben-astronomy/moon/processing_scripts/namorrodor/generate_obs_download.py /data/moon/2017/20150926_moon_121.txt 
--- moon/default_scripts/namorrodor/93/off_moon/2_default_cotter_20150929_off_moon_93.sh
+++ moon/default_scripts/namorrodor/93/off_moon/2_default_cotter_20150929_off_moon_93.sh
@@ -1,1 +1,1 @@
-python /group/mwaeor/CODE/MWA_Tools/scripts/generate_cotter_moon.py --tagname=20150929_moon_93 --track_off_moon=track_off_moon_20150926_93.txt --flag_ants=" 56 60 " 20150929_off_moon1_93.txt  
+python /data/code/git/ben-astronomy/moon/processing_scripts/namorrodor/generate_cotter_moon.py --cleanup --tagname=20150929_moon_93 --track_off_moon=/data/moon/2017/track_off_moon_20150926_93.txt --flag_ants=" 56 60 " /data/moon/2017/20150929_off_moon1_93.txt  
