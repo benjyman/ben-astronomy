4199a79eecc7b4302a742df215c37e2372607d6e
benjamin <benjamin@namorrodor>
2017-08-04 10:54:17 +0800

    default galaxy changes paths

--- moon/default_scripts/galaxy/145/off_moon/1_default_cotter_20150929_off_moon_145.sh
+++ moon/default_scripts/galaxy/145/off_moon/1_default_cotter_20150929_off_moon_145.sh
@@ -1,1 +1,1 @@
-python /group/mwaeor/CODE/MWA_Tools/scripts/generate_cotter_moon.py --tagname=20150929_moon_145 --track_off_moon=track_off_moon_20150926_145.txt --flag_ants=" 56 60 "  20150929_off_moon1_145.txt 
+python /group/mwaeor/bmckinley/git_repos/ben-astronomy/moon/processing_scripts/galaxy/generate_cotter_moon.py --tagname=20150929_moon_145 --track_off_moon=track_off_moon_20150926_145.txt --flag_ants=" 56 60 "  --cleanup 20150929_off_moon1_145.txt 
