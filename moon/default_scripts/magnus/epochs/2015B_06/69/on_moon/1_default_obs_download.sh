a3bd17a7f7a6463970cc96c7f9185ffd35cbc0fc
Benjamin McKinley <benjyman1024@gmail.com>
2018-03-01 15:02:54 +0800

    added code for moon processing on magnus

---
+++ moon/default_scripts/magnus/epochs/2015B_06/69/on_moon/1_default_obs_download.sh
@@ -0,0 +1,1 @@
+python /astro/mwaeor/bmckinley/code/ben-astronomy/moon/processing_scripts/namorrodor_magnus/generate_obs_download.py 2015B_06_on_moon_69.txt --machine="magnus" 
--- moon/processing_scripts/namorrodor_magnus/generate_obs_download.py
+++ moon/processing_scripts/namorrodor_magnus/generate_obs_download.py
@@ -4,7 +4,16 @@ import os
 import numpy as np
 from distutils import command
 
-def generate_namorrodor(infile,options):
+def generate_download_obs(infile,options): 
+   machine=options.machine.strip()
+   if machine=="namorrodor":
+      mwa_dir = "/md0/moon/data/MWA/"
+      code_dir="/data/code/git/MWA_Tools/scripts/"
+   elif  machine=="magnus":
+      mwa_dir= "/astro/mwaeor/MWA/data/"
+      code_dir=""
+   else:
+      print "machine does not match any known"   
 
    chunk_size=20
    obsid_list=[]
@@ -23,7 +32,6 @@ def generate_namorrodor(infile,options):
    for obs_list_number in range(n_obs_lists):
 
       imaging_file = open('q_obsdownload_wrapper_chunk_%s.sh' % (str(obs_list_number)),'w+')
-      imaging_file.write('cd %s\n' % mwa_dir)
       
       start_obs_id_index=int(obs_list_number*chunk_size)
 
@@ -31,6 +39,27 @@ def generate_namorrodor(infile,options):
           end_obs_id_index=int(obs_list_number*chunk_size+chunk_size)
       else:
           end_obs_id_index=int(obs_list_number*chunk_size+last_list_length)
+
+      if machine=="magnus":
+         imaging_file.write('#!/bin/bash -l\n')
+         imaging_file.write('#SBATCH -o getNGASdata-%A.out\n' )
+         imaging_file.write('##SBATCH --ntasks=1\n')
+         imaging_file.write('#SBATCH --ntasks-per-node=1\n')
+         imaging_file.write('#SBATCH --time=12:00:00\n')
+         imaging_file.write('#SBATCH -J download_obs\n')
+         #imaging_file.write('#SBATCH --array=0-%s\n' % (n_obs-1))
+         #imaging_file.write('#SBATCH --clusters=magnus\n')
+         imaging_file.write('#SBATCH --partition=workq\n')
+         imaging_file.write('#SBATCH --account=mwaeor\n')
+         imaging_file.write('#SBATCH --export=NONE\n')
+         #imaging_file.write('module swap PrgEnv-cray/6.0.4 PrgEnv-gnu && module swap gcc gcc/5.3.0\n')
+         #imaging_file.write('module use /group/mwa/software/modulefiles\n')
+         #imaging_file.write('module load MWA_Tools/mwa-sci\n')
+         #imaging_file.write('module load setuptools\n')
+         imaging_file.write('cd %s\n' % mwa_dir)
+
+      elif machine=="namorrodor":
+         imaging_file.write('cd %s\n' % mwa_dir)
 
       for obsid in obsid_list[start_obs_id_index:end_obs_id_index]:
          imaging_file.write('%sobsdownload.py -o %s\n' % (code_dir,obsid))
@@ -52,18 +81,17 @@ def generate_namorrodor(infile,options):
 import sys,os, glob
 from optparse import OptionParser,OptionGroup
 
-mwa_dir = "/md0/moon/data/MWA/"
-code_dir="/data/code/git/MWA_Tools/scripts/"
-
 usage = 'Usage: generate_obs_download.py [text file of obsIDs]'
 
 parser = OptionParser(usage=usage)
+
+parser.add_option('--machine',type='string', dest='machine',default='magnus',help='machine being used for processing, current options are namorrodor and magnus [default=%default]')
 
 (options, args) = parser.parse_args()
 
 infile = args[0]
   
-generate_namorrodor(infile,options)
+generate_download_obs(infile,options)
 
 
 
