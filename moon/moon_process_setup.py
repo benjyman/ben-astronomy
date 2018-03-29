f4863b4173af19a8d70963edd62896d50581c6ac
benjamin <benjyman1024@gmail.com>
2018-03-29 12:06:50 +0800

    off_moon !

--- moon/moon_process_setup.py
+++ moon/moon_process_setup.py
@@ -39,7 +39,7 @@ def write_and_run_default_scripts(epoch_ID,chan,on_off_moon_dir,machine):
    on_off_moon_string=on_off_moon_dir.strip().split('/')[-2]
    if on_off_moon_string=='on_moon':
       observations_filename="%s%s_on_moon_%s.txt" % (on_off_moon_dir,epoch_ID,chan)   
-      off_moon_dir=os.path.dirname(os.path.dirname(on_off_moon_dir))+'/on_moon/'
+      off_moon_dir=os.path.dirname(os.path.dirname(on_off_moon_dir))+'/off_moon/'
       sister_observations_filename="%s%s_off_moon_%s.txt" % (off_moon_dir,epoch_ID,chan)    
       track_moon_string='--track_moon'     
    elif on_off_moon_string=='off_moon':
