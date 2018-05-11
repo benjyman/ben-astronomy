00fe805d92ea2203ed0e70862e6accca5f72c07c
Benjamin McKinley <benjyman1024@gmail.com>
2018-05-11 19:08:20 +0800

    30 mins q for pbcorr array job

--- moon/processing_scripts/namorrodor_magnus/generate_qpbcorr_multi.py
+++ moon/processing_scripts/namorrodor_magnus/generate_qpbcorr_multi.py
@@ -145,7 +145,10 @@ def generate_namorrodor(options):
        sbatch_file.write('#SBATCH -o pbcorr-%A.out\n' )
        sbatch_file.write('##SBATCH --ntasks=1\n')
        sbatch_file.write('#SBATCH --ntasks-per-node=1\n')
-       sbatch_file.write('#SBATCH --time=06:00:00\n')
+       if options.array_by_chan:
+          sbatch_file.write('#SBATCH --time=00:30:00\n'
+       else:
+          sbatch_file.write('#SBATCH --time=06:00:00\n')
        sbatch_file.write('#SBATCH -J pbcorr_%s\n' % (options.epoch_ID))
        if options.array_by_chan:
           sbatch_file.write('#SBATCH --array=0-%s\n' % (n_array_jobs-1))
