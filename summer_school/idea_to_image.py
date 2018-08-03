a85179d3e38436c94fc2dca80a64c8c1e3a7db1f
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2018-08-03 14:35:03 +0800

    idea to image up to imaging fix wsclean cmd

--- summer_school/idea_to_image.py
+++ summer_school/idea_to_image.py
@@ -120,7 +120,7 @@ def idea_to_image(options):
       lambda_on_D_deg = (wavelength/max_baseline)/np.pi*180
       scale=lambda_on_D_deg/oversample
       print scale
-      cmd = "wsclean -name  %s -size 2000 2000  -niter 5000 -threshold 0.3 -multiscale -mgain 0.85 -datacolumn CORRECTED_DATA  -scale %.3f -weight briggs 1 -smallinversion  -make-psf    -pol xx,xy,yx,yy -joinpolarizations  %s.ms" % (obs,scale,obs)
+      cmd = "wsclean -name  %s -size 2000 2000  -niter 5000 -threshold 0.3 -multiscale -mgain 0.85 -data-column CORRECTED_DATA  -scale %.3f -weight briggs 1 -small-inversion  -make-psf    -pol xx,xy,yx,yy -join-polarizations  %s.ms" % (obs,scale,obs)
       print cmd
       os.system(cmd)
 
