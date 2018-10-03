1da92b744919dc8304ad3541ab7ab769dc059b13
Benjamin McKinley <benjyman1024@gmail.com>
2018-10-03 09:36:40 +0800

    cahnge back rename

--- moon/moon_process_setup.py
+++ moon/moon_process_setup.py
@@ -496,12 +496,12 @@ def setup_moon_process(options):
                   new_on_moon_ms_name = '%s%s/%s_%s_trackmoon.ms' % (mwa_dir,on_moon_obsid,on_moon_obsid,epoch_ID)
                   new_off_moon_ms_name = '%s%s/%s_%s_track_off_moon_paired_%s.ms' % (mwa_dir,off_moon_obsid,off_moon_obsid,epoch_ID,on_moon_obsid)
 
-                  cmd = "rm -rf %s" % (new_on_moon_ms_name)
-                  print cmd 
-                  os.system(cmd)
-                  cmd = "rm -rf %s" % (new_off_moon_ms_name)
-                  print cmd 
-                  os.system(cmd)                  
+                  #cmd = "rm -rf %s" % (new_on_moon_ms_name)
+                  #print cmd 
+                  #os.system(cmd)
+                  #cmd = "rm -rf %s" % (new_off_moon_ms_name)
+                  #print cmd 
+                  #os.system(cmd)                  
                   cmd = "mv %s %s" % (old_on_moon_ms_name,new_on_moon_ms_name)
                   print cmd 
                   os.system(cmd)
