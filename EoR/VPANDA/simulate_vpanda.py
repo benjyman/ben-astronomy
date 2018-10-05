e5c256320342997b555539e80c422abd1c2b52a7
Benjamin <benjyman1024@gmail.com>
2018-10-05 10:40:41 +0800

    add folder in EOR for VPANDA

---
+++ EoR/VPANDA/simulate_vpanda.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#1. design sky model with one source above and one below horizon
+#2. test with OSKAR test example and wsclean
+#3. Design one iteration of Moon ULFA
+#4. Repeat tests
+#5. write program to generate telescope iterations
+#6. Combine iterations and test imaging algorithms
+#7. Add noise, test calibration 
