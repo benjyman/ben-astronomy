474c3ab26b87332836ecb69a8840c86559ce1378
Benjamin <benjyman1024@gmail.com>
2019-03-07 15:29:30 +0800

    added notes to assassin.py

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -1,6 +1,20 @@
 #ASSASSIN: (All Sky SigAl Short SPacing INterferometer)
 #Script to replicate Cath's global step simulations
 
+#Generate model visibilities
+#1. Get a sky model (healpix map of GSM2017) - easy
+#2. Get a beam model in healpix (not entirely straightforward, but I think I have done this before) just use short dipole model.
+#3. Generate a point source model (I don't understand how this is done)
+#4. Generate a global EoR signal (I think I understand how to do this from Cath's eqn 3 etc
+ 
+#Get EDA antenna positions (you have these)
+
+#Use zenith pointing centre and phase centre of RA = 4h, Dec.= −27 degrees
+
+#Generate simulated visibilities ... with miriad to start?
+
+
+
 
 
 
