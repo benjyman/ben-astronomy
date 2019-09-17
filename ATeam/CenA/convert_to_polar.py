522491ed11e3863145776a7043a88a1c2813c671
Benjamin <benjyman1024@gmail.com>
2019-09-17 14:36:41 +0800

    polar coord stuff from internet

--- ATeam/CenA/convert_to_polar.py
+++ ATeam/CenA/convert_to_polar.py
@@ -1,2 +1,25 @@
+#!/usr/bin/env python
 #convert to polar coords
+#see https://stackoverflow.com/questions/51675940/converting-an-image-from-cartesian-to-polar-limb-darkening
+import matplotlib
+matplotlib.use('Agg')
 
+import cv2
+import numpy as np
+
+source = cv2.imread('C:/Users/selwyn77/Desktop/sun.jpg', 1)
+
+#--- ensure image is of the type float ---
+img = source.astype(np.float32)
+
+#--- the following holds the square root of the sum of squares of the image dimensions ---
+#--- this is done so that the entire width/height of the original image is used to express the complete circular range of the resulting polar image ---
+value = np.sqrt(((img.shape[0]/2.0)**2.0)+((img.shape[1]/2.0)**2.0))
+
+polar_image = cv2.linearPolar(img,(img.shape[0]/2, img.shape[1]/2), value, cv2.WARP_FILL_OUTLIERS)
+
+polar_image = polar_image.astype(np.uint8)
+cv2.imshow("Polar Image", polar_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- ATeam/CenA/split_CenA_obs_by_time.py
+++ ATeam/CenA/split_CenA_obs_by_time.py
@@ -3,47 +3,89 @@
 import os
 import datetime
 
-time_to_split_off_s = 8.
+time_to_split_off_s = 16.
 
-year = '2015'
-chan = '145'
-obsid = '1117031728'
+year_list = ['2015','2018']
+chan_list = ['145','169']
 
-input_ms_name = "/md0/ATeam/CenA/%s/%s/%s/%s.ms" % (year,chan,obsid,obsid)
-output_ms_name = "/md0/ATeam/CenA/%s/%s/%s/%s_split.ms" % (year,chan,obsid,obsid)
-listfile_name = "/md0/ATeam/CenA/%s_info.txt" % obsid
-casa_script_filename = "casa_cmd.sh"
-casa_cmd_string = "listobs(vis='%s',listfile='%s')" % (input_ms_name,listfile_name)
+#year = '2015'
+#chan = '145'
+#obsid_list = ['1117031728','1121334536','1121420968','1121507392','1121593824','1121680256']
 
-with open(casa_script_filename,'w') as f:
-   f.write(casa_cmd_string)
+#year = '2015'
+#chan = '169'
+#obsid_list = ['1117031848','1121248288','1121334712','1121421144','1121507576','1121594000']
 
-cmd = "rm -rf %s" % listfile_name
-print cmd
-os.system(cmd)
+#year = '2018'
+#chan = '145'
+#obsid_list = ['1199663088','1200604688','1200691136','1200777584','1200864032','1200950480']
 
-cmd = "casa -nologger -nogui -c %s" % casa_script_filename
-print cmd
-os.system(cmd)
+#year = '2018'
+#chan = '169'
+#obsid_list = ['1200431968','1200518416','1200604864','1200691312','1200777760','1200864208']
 
-with open(listfile_name,'r') as f:
-   lines = f.readlines()
-
-for line in lines:
-   if ("Observed from" in line):
-      start_time = line.split('from')[1].split()[0].split('.')[0]
-
-#30-May-2015/14:35:13.0
-start_time_datetime = datetime.datetime.strptime(start_time, "%d-%b-%Y/%H:%M:%S") 
-time_to_split_off_s_timedelta = datetime.timedelta(seconds=time_to_split_off_s)
-end_time_datetime = start_time_datetime + time_to_split_off_s_timedelta
-
-#'YYYY/MM/DD/hh:mm:ss~YYYY/MM/DD/hh:mm:ss'
-start_time_string_casa = start_time_datetime.strftime("%Y/%m/%d/%H:%M:%S")
-end_time_string_casa = end_time_datetime.strftime("%Y/%m/%d/%H:%M:%S")
-
-timerange_string_casa = "%s~%s" % (start_time_string_casa,end_time_string_casa)
-
-print timerange_string_casa
-
-
+for year in year_list:
+   for chan in chan_list:
+      if year=='2015':
+         if chan=='145':
+            obsid_list = ['1117031728','1121334536','1121420968','1121507392','1121593824','1121680256']
+         elif chan=='169':
+            obsid_list = ['1117031848','1121248288','1121334712','1121421144','1121507576','1121594000']
+      elif year=='2018':
+         if chan=='145':
+            obsid_list = ['1199663088','1200604688','1200691136','1200777584','1200864032','1200950480']
+         elif chan=='169':
+            obsid_list = ['1200431968','1200518416','1200604864','1200691312','1200777760','1200864208']
+      
+      for obsid in obsid_list:
+         input_ms_name = "/md0/ATeam/CenA/%s/%s/%s/%s.ms" % (year,chan,obsid,obsid)
+         output_ms_name = "/md0/ATeam/CenA/%s/%s/%s/%s_split.ms" % (year,chan,obsid,obsid)
+         
+         
+         listfile_name = "/md0/ATeam/CenA/%s_info.txt" % obsid
+         casa_script_filename = "casa_cmd.sh"
+         casa_cmd_string = "listobs(vis='%s',listfile='%s')" % (input_ms_name,listfile_name)
+         with open(casa_script_filename,'w') as f:
+            f.write(casa_cmd_string)
+         
+         cmd = "rm -rf %s" % listfile_name
+         print cmd
+         os.system(cmd)
+         
+         cmd = "casa -nologger -nogui -c %s" % casa_script_filename
+         print cmd
+         os.system(cmd)
+         
+         with open(listfile_name,'r') as f:
+            lines = f.readlines()
+         
+         for line in lines:
+            if ("Observed from" in line):
+               start_time = line.split('from')[1].split()[0].split('.')[0]
+         
+         #30-May-2015/14:35:13.0
+         start_time_datetime = datetime.datetime.strptime(start_time, "%d-%b-%Y/%H:%M:%S") 
+         time_to_split_off_s_timedelta = datetime.timedelta(seconds=time_to_split_off_s)
+         end_time_datetime = start_time_datetime + time_to_split_off_s_timedelta
+         
+         #'YYYY/MM/DD/hh:mm:ss~YYYY/MM/DD/hh:mm:ss'
+         start_time_string_casa = start_time_datetime.strftime("%Y/%m/%d/%H:%M:%S")
+         end_time_string_casa = end_time_datetime.strftime("%Y/%m/%d/%H:%M:%S")
+         
+         timerange_string_casa = "%s~%s" % (start_time_string_casa,end_time_string_casa)
+         
+         print timerange_string_casa
+         
+         casa_script_filename = "casa_cmd_split.sh"
+         casa_cmd_string = "split(vis='%s',outputvis='%s',timerange='%s')" % (input_ms_name,output_ms_name,timerange_string_casa)
+         with open(casa_script_filename,'w') as f:
+            f.write(casa_cmd_string)
+         
+         cmd = "rm -rf %s" % output_ms_name
+         print cmd
+         os.system(cmd)
+         
+         cmd = "casa -nologger -nogui -c %s" % casa_script_filename
+         print cmd
+         os.system(cmd)
+      
