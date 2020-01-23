0e57f28d0d72491b5c88ca2839fc51c24b80cdf2
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-01-23 16:14:00 +0530

    use concat instead of av in solve from uvfits

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -8401,7 +8401,7 @@ EDA2_obs_time_list_each_chan = [
    ['20191202T172027','20191202T172032','20191202T172036','20191202T172041','20191202T172046','20191202T172051','20191202T172056','20191202T172100','20191202T172105','20191202T172110','20191202T172115','20191202T172120','20191202T172125']
    ]
 
-EDA2_obs_time_list_each_chan = EDA2_obs_time_list_each_chan[3:]
+EDA2_obs_time_list_each_chan = EDA2_obs_time_list_each_chan[0:]
 
 n_obs_concat_list = [len(obs_list) for obs_list in EDA2_obs_time_list_each_chan] 
 
@@ -8416,8 +8416,8 @@ EDA2_chan_list_array = np.asarray(EDA2_chan_list)
 eda2_data_dir = '/md0/EoR/EDA2/20191213_data/'
 EDA2_obs_time = '20191202T171727'
 freq_MHz_array = np.round(400./512.*EDA2_chan_list_array)
-freq_MHz_list = freq_MHz_array[3:]
-EDA2_chan_list = EDA2_chan_list[3:]
+freq_MHz_list = freq_MHz_array[0:]
+EDA2_chan_list = EDA2_chan_list[0:]
 
 
 baseline_length_thresh_lambda = 0.50
