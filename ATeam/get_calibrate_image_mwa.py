9a29aa397ecaab3a8686897b958f2ce2506e6d9e
Benjamin <benjyman1024@gmail.com>
2020-02-07 11:41:15 +0800

    download obs

--- ATeam/get_calibrate_image_mwa.py
+++ ATeam/get_calibrate_image_mwa.py
@@ -10,7 +10,7 @@ import os,sys
 obsid_list_2015 = ['1122198832','1122112400','1122025976','1121939544','1121853112','1121766680']
 obsid_list_2018 = ['1201123376','1201296272','1201382720','1201469160','1201555608','1201814952']
 
-def download_obs(obsid_list,timeres=4,freqres=40,ms=True)
+def download_obs(obsid_list,timeres=4,freqres=40,ms=True):
    #write the csv file
    csv_filename = "manta_ray_download.csv"
    for obsid in obsid_list:
