aff5ecad77584e235767fb5d45e3ad1bdd39c767
Benjamin <benjyman1024@gmail.com>
2020-03-03 15:50:28 +0800

    ozstar script sbatch launch dependencies

--- ATeam/ozstar_scripts/generate_ozstar_scripts.py
+++ ATeam/ozstar_scripts/generate_ozstar_scripts.py
@@ -273,7 +273,7 @@ def generate_sbatch_script_CenA(obsid_list,ms_dir_list,n_selfcals,download=False
          selfcal_out_filename_list = generate_selfcal(obsid_list,ms_dir_list,calibrate_options=calibrate_options_1,self_cal_number=selfcal+1,dest_dir='/fred/oz048/bmckinle/ATeam/CenA/image4')
          
          for selfcal_out_filename_index,selfcal_out_filename in enumerate(selfcal_out_filename_list):
-            selfcal_job_index = selfcal*len(selfcal_out_filename_list) + selfcal_out_filename_index + 1
+            selfcal_job_index = selfcal*len(selfcal_out_filename_list) + selfcal_out_filename_index + 1 + selfcal
             cmd = 'jid%s=$(sbatch --dependency=afterok:$jid%s %s) \n' % (selfcal_job_index,wsclean_job_index,selfcal_out_filename)
             cmd_list.append(cmd)
             job_id_list.append('$jid%s' % selfcal_job_index)
