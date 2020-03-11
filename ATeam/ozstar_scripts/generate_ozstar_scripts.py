f9b5188c8db9b9bacd3d87648cb58a3530cc0043
Benjamin <benjyman1024@gmail.com>
2020-03-11 09:34:13 +0800

    added all obsid up to image 8

--- ATeam/ozstar_scripts/generate_ozstar_scripts.py
+++ ATeam/ozstar_scripts/generate_ozstar_scripts.py
@@ -131,7 +131,7 @@ def generate_model_cal(obsid_list,model_wsclean_txt='',dest_dir=''):
 def generate_wsclean_image(obsid_list,ms_dir_list,out_image_name_base,wsclean_options,dest_dir,self_cal_number=0):
    print('generating wsclean script')
    out_filename = '%s/wsclean_selfcal_%02d.sh' % (dest_dir,self_cal_number)
-   initiate_script(out_filename,time_hrs=15,partition_string='skylake-gpu')
+   initiate_script(out_filename,time_hrs=20,partition_string='skylake-gpu')
    ms_list = []
    cmd_list = []
    for ms_dir in ms_dir_list:
@@ -291,7 +291,7 @@ def generate_sbatch_script_CenA(image_number,n_selfcals,download=False,model_cal
       #final robust 0 clean
       out_image_name_base = "image_%02d_selfcal_%02d_robust0" % (image_number,selfcal+1)
       
-      wsclean_options_2 = "-size 4096 4096 -j 8 -mwa-path /fred/oz048/MWA/CODE/MWA_Tools/mwapy/data -niter 350000 -threshold 0.015  -multiscale -mgain 0.85 -save-source-list -data-column CORRECTED_DATA -scale 0.004 -weight briggs 0  -small-inversion -make-psf -pol I -use-idg -grid-with-beam -idg-mode hybrid -pb-undersampling 4 -channels-out 10 -join-channels -fit-spectral-pol 2"
+      wsclean_options_2 = "-size 4096 4096 -j 8 -mwa-path /fred/oz048/MWA/CODE/MWA_Tools/mwapy/data -auto-threshold 1 -auto-mask 3 -multiscale -niter 1000000 -mgain 0.85 -save-source-list -data-column CORRECTED_DATA -scale 0.004 -weight briggs 0  -small-inversion -make-psf -pol I -use-idg -grid-with-beam -idg-mode hybrid -pb-undersampling 4 -channels-out 10 -join-channels -fit-spectral-pol 2"
       wsclean_out_filename = generate_wsclean_image(obsid_list,ms_dir_list,out_image_name_base=out_image_name_base,wsclean_options=wsclean_options_2,dest_dir='/fred/oz048/bmckinle/ATeam/CenA/image%s' % image_number,self_cal_number=n_selfcals)
       cmd = 'id%s=$(sbatch --dependency=afterok:$jid%s %s | cut -f 4 -d " ") \n' % (selfcal_job_index+1,job_id_list_string,wsclean_out_filename)
       cmd_list.append(cmd)
