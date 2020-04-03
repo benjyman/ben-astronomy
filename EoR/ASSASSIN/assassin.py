8f3bb2952df64ce4702e8512084d3e2c2ddaea8d
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-04-03 10:31:40 +0800

    do calibration with a model with fine-channel frequency info channels-out 32

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -8946,7 +8946,7 @@ def calibrate_eda2_data(EDA2_chan_list,obs_type='night',lst_list=[],pol_list=[],
                   print(cmd)
                   os.system(cmd)
                   
-                  sys.exit()
+                  #crikey I think it actually works
                   
                   #hmmm seemed to actually work! We'll see ...
                   if uv_cutoff==0:
