40e27ff8b9e9e7ebc5022e4e98146485e9279f95
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-04-06 13:42:32 +0800

    chan 70 prob

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -2043,9 +2043,11 @@ def model_tsky_from_saved_data(freq_MHz_list,freq_MHz_index,lst_hrs,pol,signal_t
       t_sky_jy = parameters[0]
       t_sky_error_jy = results.bse[0]
       
+      X_short_parallel_array_nonans_nonans = X_short_parallel_array_nonans[np.logical_not(np.isnan(real_vis_data_sorted_array_flagged))]
+      
       plt.clf()
       plt.plot(X_short_parallel_array, real_vis_data_sorted_array_flagged,label='%s data' % real_or_simulated_string,linestyle='None',marker='.')
-      plt.plot(X_short_parallel_array_nonans, results.fittedvalues, 'r--.', label="OLS fit",linestyle='--',marker='None')
+      plt.plot(X_short_parallel_array_nonans_nonans, results.fittedvalues, 'r--.', label="OLS fit",linestyle='--',marker='None')
    
       map_title="Flagged data and fit" 
       plt.xlabel("Expected global-signal response")
