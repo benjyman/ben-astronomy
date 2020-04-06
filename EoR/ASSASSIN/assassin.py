a058b1753eefefca84b5fca78f75704106e8c83a
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-04-06 10:08:07 +0800

    chan 70 prob

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -2024,9 +2024,7 @@ def model_tsky_from_saved_data(freq_MHz_list,freq_MHz_index,lst_hrs,pol,signal_t
    real_vis_data_sorted_array_flagged[np.abs(real_vis_data_sorted_array_subtr_model) > thresh] = np.nan
    
    X_short_parallel_array_flagged = np.copy(X_short_parallel_array)
-   X_short_parallel_array_flagged[np.abs(real_vis_data_sorted_array_subtr_model) > thresh] = np.nan
-   X_short_parallel_array_flagged_removed = X_short_parallel_array_flagged[np.logical_not(np.isnan(X_short_parallel_array_flagged))]
-   
+
    model = sm.OLS(real_vis_data_sorted_array_flagged, X_short_parallel_array,missing='drop')
    results = model.fit()
    ##print results.summary()
@@ -2036,10 +2034,17 @@ def model_tsky_from_saved_data(freq_MHz_list,freq_MHz_index,lst_hrs,pol,signal_t
    t_sky_jy = parameters[0]
    t_sky_error_jy = results.bse[0]
    
+   print(real_vis_data_sorted_array_flagged)
+   print(real_vis_data_sorted_array_flagged.shape)
    print(X_short_parallel_array_flagged_removed)
    print(X_short_parallel_array_flagged_removed.shape)
    print(results.fittedvalues)
    print(results.fittedvalues.shape)
+   
+   X_short_parallel_array_flagged[np.abs(real_vis_data_sorted_array_subtr_model) > thresh] = np.nan
+   X_short_parallel_array_flagged_removed = X_short_parallel_array_flagged[np.logical_not(np.isnan(X_short_parallel_array_flagged))]
+     
+   
    plt.clf()
    plt.plot(X_short_parallel_array_flagged, real_vis_data_sorted_array_flagged,label='%s data' % real_or_simulated_string,linestyle='None',marker='.')
    plt.plot(X_short_parallel_array_flagged_removed, results.fittedvalues, 'r--.', label="OLS fit",linestyle='--',marker='None')
