d28473658129208f3a98616cbc4d0715285bd878
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-04-07 16:58:09 +0800

    fixing PFB overlap channel problem in plot_tsky_for_multiple_freqs, plot_only is True

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -3515,7 +3515,7 @@ def plot_tsky_for_multiple_freqs(lst_hrs_list,freq_MHz_list,pol_list,signal_type
                n_obs_concat = 1
             #fine_chan_index_array = range(n_fine_chans)[n_chans_omitted_each_edge:n_fine_chans-n_chans_omitted_each_edge]
             #omit 2 fine chans at the low end and 3 edge chans at the high end and you are sweet, no gaps
-            fine_chan_index_array = range(n_fine_chans-5)+2
+            fine_chan_index_array = np.arange(n_fine_chans-5)+2
             for fine_chan_index_index,fine_chan_index in enumerate(fine_chan_index_array):
                freq_MHz_index_fine = freq_MHz_index*n_fine_chans + fine_chan_index_index
                #oversampled PFB - dont need all this edge chan stuff
