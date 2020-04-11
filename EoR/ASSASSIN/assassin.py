b58f5ad1b4a3955920a02fc5e108a23986294fd7
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-04-11 14:52:40 +0800

    first 10 plot only wrong channel ordering

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -10478,7 +10478,7 @@ include_angular_info = True
 
 #up to here with plot_only = False
 #chan_num = 10
-#chan_num = 0
+chan_num = 0
 #freq_MHz_list = [freq_MHz_array[chan_num]]
 #EDA2_chan_list = [EDA2_chan_list[chan_num]]
 freq_MHz_list = freq_MHz_array[chan_num:chan_num+10]
