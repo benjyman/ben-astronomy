e7af55658bdc84e9ff7587b406ab6c9f909ea6e2
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-05-04 16:13:30 +0800

    it worked, per chan cal with wsclean flatten the result

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -11297,16 +11297,16 @@ for EDA2_obs_time_index,EDA2_obs_time in enumerate(EDA2_obs_time_list):
 ##calibrate each individually first and concat
 ##do this outside chan dir
 ##if doing individual chans:
-chan_num = 0
-freq_MHz_list = [freq_MHz_array[chan_num]]
-EDA2_chan_list = [EDA2_chan_list[chan_num]]
+#chan_num = 0
+#freq_MHz_list = [freq_MHz_array[chan_num]]
+#EDA2_chan_list = [EDA2_chan_list[chan_num]]
 plot_cal = False
-#wsclean = False
+##wsclean = False
 wsclean = True
 concat=True
 per_chan_cal = True
 calibrate_eda2_data(EDA2_chan_list=EDA2_chan_list,obs_type='night',lst_list=lst_hrs_list,pol_list=pol_list,n_obs_concat_list=n_obs_concat_list,concat=concat,wsclean=wsclean,plot_cal=plot_cal,uv_cutoff=0,per_chan_cal=per_chan_cal)
-#sys.exit()
+sys.exit()
 
 #Need to plug in monitor to namorrodor, can't do this with nohup or remotely
 #make_image_movie_from_ds9(EDA2_chan_list,n_obs_concat_list,'20200303_data.mp4')
