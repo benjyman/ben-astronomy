15182b6c3907339dfff22f7f13b322ca3f0059c4
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-05-04 10:50:16 +0800

    try to get wsclean working agin, redo first chan normal -ch 32 chan cal for referecnce not plot only 0.25 thresh

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -9262,7 +9262,7 @@ def plot_EDA2_cal_sols(EDA2_chan,EDA2_obs_time,fine_chan_index,phase_sol_filenam
    
    
    
-def calibrate_eda2_data(EDA2_chan_list,obs_type='night',lst_list=[],pol_list=[],sky_model_im_name='',n_obs_concat_list=[],concat=False,wsclean=False,plot_cal=False,uv_cutoff=0):
+def calibrate_eda2_data(EDA2_chan_list,obs_type='night',lst_list=[],pol_list=[],sky_model_im_name='',n_obs_concat_list=[],concat=False,wsclean=False,plot_cal=False,uv_cutoff=0,per_chan_cal=False):
    #specify uv_cutoff in wavelengths, convert to m for 'calibrate'
    pol = pol_list[0]
    for EDA2_chan_index,EDA2_chan in enumerate(EDA2_chan_list):  
@@ -10184,6 +10184,27 @@ def calibrate_eda2_data(EDA2_chan_list,obs_type='night',lst_list=[],pol_list=[],
              print(cmd)
              os.system(cmd)
 
+
+             #calibrate per chan on concat ms
+             if per_chan_cal==True:
+                if uv_cutoff==0:
+                   gain_solutions_name = 'cal_%s_%s_calibrate_sols_per_chan.bin' % (EDA2_chan,EDA2_obs_time)
+                   calibrate_options = ''
+                else:
+                   gain_solutions_name = 'cal_%s_%s_calibrate_sols_uvcutoff_%0.3f_m_per_chan.bin' % (EDA2_chan,EDA2_obs_time,uv_cutoff_m)
+                   calibrate_options = '-minuv %0.3f ' % uv_cutoff_m
+
+                #calibrate on each chan and use the corrected data
+                cmd = "calibrate  -ch 1 -datacolumn CORRECTED_DATA %s %s %s " % (calibrate_options,concat_ms_name_wsclean_cal,gain_solutions_name)
+                print(cmd)
+                os.system(cmd)
+                
+                #apply sols
+                cmd = "applysolutions %s %s " % (concat_ms_name_wsclean_cal,gain_solutions_name)
+                print(cmd)
+                os.system(cmd)
+                
+                
              cmd = "rm -rf %s " % (casa_cmd_filename)
              print(cmd)
              os.system(cmd)
@@ -10204,12 +10225,13 @@ def calibrate_eda2_data(EDA2_chan_list,obs_type='night',lst_list=[],pol_list=[],
              print(cmd)
              os.system(cmd)
 
+
              #image the concat file to check spectral behaviour
              ##make an image to check (both pols) 32 chans
              cmd = "wsclean -name concat_cal_chan_%s_ms -size %s %s -auto-threshold 5 -scale %s -pol xx,yy -data-column CORRECTED_DATA -channels-out 32 %s " % (EDA2_chan,wsclean_imsize,wsclean_imsize,wsclean_scale,concat_ms_name_wsclean_cal)
              print(cmd)
-             os.system(cmd) 
-    
+             os.system(cmd)
+                          
           else:
              print("concatenating calibrated data into one vis and uvfits with miriad")
              
@@ -11279,7 +11301,8 @@ for EDA2_obs_time_index,EDA2_obs_time in enumerate(EDA2_obs_time_list):
 ##wsclean = False
 #wsclean = True
 #concat=True
-#calibrate_eda2_data(EDA2_chan_list=EDA2_chan_list,obs_type='night',lst_list=lst_hrs_list,pol_list=pol_list,n_obs_concat_list=n_obs_concat_list,concat=concat,wsclean=wsclean,plot_cal=plot_cal,uv_cutoff=0)
+per_chan_cal = True
+#calibrate_eda2_data(EDA2_chan_list=EDA2_chan_list,obs_type='night',lst_list=lst_hrs_list,pol_list=pol_list,n_obs_concat_list=n_obs_concat_list,concat=concat,wsclean=wsclean,plot_cal=plot_cal,uv_cutoff=0,per_chan_cal=per_chan_cal)
 #sys.exit()
 
 #Need to plug in monitor to namorrodor, can't do this with nohup or remotely
@@ -11344,22 +11367,22 @@ model_type_list = ['OLS_fixed_intercept']
 #poly_order_list=[5,6,7]
 #poly_order=7
 
-plot_only = True
-baseline_length_thresh_lambda = 0.23
+plot_only = False
+baseline_length_thresh_lambda = 0.25
 include_angular_info = True
 
 
 #up to here with plot_only = False
 #chan_num = 10
 chan_num = 0
-#freq_MHz_list = [freq_MHz_array[chan_num]]
-#EDA2_chan_list = [EDA2_chan_list[chan_num]]
-freq_MHz_list = freq_MHz_array[chan_num:chan_num+2]
-EDA2_chan_list = EDA2_chan_list[chan_num:chan_num+2]
+freq_MHz_list = [freq_MHz_array[chan_num]]
+EDA2_chan_list = [EDA2_chan_list[chan_num]]
+#freq_MHz_list = freq_MHz_array[chan_num:chan_num+2]
+#EDA2_chan_list = EDA2_chan_list[chan_num:chan_num+2]
 #wsclean=False # for sims or miriad cal
 wsclean=True # for data
 fast=False
-no_modelling=True
+no_modelling=False
 plot_tsky_for_multiple_freqs(lst_hrs_list=lst_hrs_list,freq_MHz_list=freq_MHz_list,pol_list=pol_list,signal_type_list=signal_type_list,sky_model=sky_model,array_label=array_label,baseline_length_thresh_lambda=baseline_length_thresh_lambda,poly_order=poly_order,plot_only=plot_only,include_angular_info=include_angular_info,model_type_list=model_type_list, EDA2_data=EDA2_data,EDA2_chan_list=EDA2_chan_list,n_obs_concat_list=n_obs_concat_list,wsclean=wsclean,fast=fast,no_modelling=no_modelling)
 
 
