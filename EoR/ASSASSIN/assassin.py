d2f10ec296d07b2214125556501bb10e56923728
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-05-26 15:05:21 +0800

    redo x_y_plots with temp units K, sims, global_EDGES, 70MHz

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -11686,7 +11686,7 @@ include_angular_info = True
 
 
 #up to here with plot_only = False
-chan_num = 26
+chan_num = 20
 #chan_num = 46
 freq_MHz_list = [freq_MHz_array[chan_num]]
 EDA2_chan_list = [EDA2_chan_list[chan_num]]
