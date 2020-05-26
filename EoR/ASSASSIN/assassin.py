2c5f17e52f09bb7261a14b2e12b2c9339c6d33bb
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-05-26 17:03:08 +0800

    redo x_y_plots with temp units K, EDA2 data, 70MHz

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -1940,8 +1940,8 @@ def model_tsky_from_saved_data_eda2(freq_MHz_list,freq_MHz_index,lst_hrs_list,po
                y_pos = np.max(results.fittedvalues)
                x_pos = 1.2 * np.min(X_short_parallel_array)
             
-               y_pos_K = np.max(fitted_values_K)
-               x_pos_K = 1.2 * np.min(X_short_parallel_array)
+               y_pos_K = 1200 #np.max(fitted_values_K)
+               x_pos_K = 0.45 # 1.2 * np.min(X_short_parallel_array)
             
                #plot in Jy
                plt.clf()
@@ -1952,7 +1952,7 @@ def model_tsky_from_saved_data_eda2(freq_MHz_list,freq_MHz_index,lst_hrs_list,po
                plt.xlabel("Expected global-signal response")
                plt.ylabel("Real component of visibility (Jy) flagged")
                plt.legend(loc=1)
-               plt.text(x_pos, y_pos, fit_string)
+               plt.text(x_pos_K, y_pos_K, fit_string)
                #plt.ylim([0, 3.5])
                fig_name= "x_y_OLS_plot_%0.3f_MHz_%s_pol%s_%s_flagged.png" % (freq_MHz_fine_chan,pol,EDA2_obs_time,model_type)
                figmap = plt.gcf()
