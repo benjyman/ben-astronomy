fee2eafcfe54ddbc2dd76ffc6ddda80efc481ad5
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-06-04 21:07:03 +0800

    making expected noise plot, no A_eff

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -5057,21 +5057,21 @@ def plot_tsky_for_multiple_freqs(lst_hrs_list,freq_MHz_list,pol_list,signal_type
          freq_MHz_fine_array = np.load(freq_MHz_fine_array_filename)
          freq_MHz_fine_array = freq_MHz_fine_array[0:length_freq_MHz_fine_chan_to_plot]
          
-         #print(length_freq_MHz_fine_chan_to_plot)
-         #print(t_sky_measured_array)
-         #print(freq_MHz_fine_array)
+         print(length_freq_MHz_fine_chan_to_plot)
+         print(t_sky_measured_array)
+         print(freq_MHz_fine_array)
         
          for freq_MHz_index,freq_MHz in enumerate(freq_MHz_list):
             freq_range_min = freq_MHz - (centre_chan_index * fine_chan_width_Hz/1000000.)
             freq_range_max = freq_MHz + (centre_chan_index * fine_chan_width_Hz/1000000.)
-            #print(freq_range_min)
-            #print(freq_range_max)
+            print(freq_range_min)
+            print(freq_range_max)
             
             indices = np.where(np.logical_and(freq_MHz_fine_array>=freq_range_min,freq_MHz_fine_array<=freq_range_max))
             t_sky_measured_EDA2_chan = t_sky_measured_array[indices]
             t_sky_measured_error_chan = t_sky_measured_error_array[indices]
-            #print(t_sky_measured_EDA2_chan)
-            #print(freq_MHz_fine_array[indices])
+            print(t_sky_measured_EDA2_chan)
+            print(freq_MHz_fine_array[indices])
             t_sky_measure_av_per_EDA2_chan[freq_MHz_index] = np.nanmean(t_sky_measured_EDA2_chan)
             t_sky_measure_av_per_EDA2_chan_err[freq_MHz_index] = np.nanstd(t_sky_measured_EDA2_chan)
           
@@ -5109,9 +5109,10 @@ def plot_tsky_for_multiple_freqs(lst_hrs_list,freq_MHz_list,pol_list,signal_type
       map_title="Residual for log polynomial order %s fit " % poly_order
       plt.ylabel("Residual Tb (K)")
       plt.xlabel("freq (MHz)")
-      if len(model_type_list)>1:
-         plt.legend(loc=1)
+      #if len(model_type_list)>1:
+      #   plt.legend(loc=1)
       
+      plt.legend(loc=1)
       plt.ylim([y_min, y_max])
       fig_name= "eda2_log_fit_residual_tsy_measured_poly_%s_lst_%s%s_no_e_bars_per_chan_av.png" % (poly_order,lst_string,signal_type_postfix)
       figmap = plt.gcf()
