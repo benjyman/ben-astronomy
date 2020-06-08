2c49cc31b51e2f1c9415428ec3b1a7322bec2f70
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-06-08 09:49:00 +0800

    redo fig3 paper1 for color blind

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -185,8 +185,8 @@ pol_list = ['X']
 #signal_type_list=['diffuse','noise']
 #signal_type_list=['diffuse']
 #signal_type_list=['global_unity']
-signal_type_list=['diffuse_global','noise']
-#signal_type_list=['global_EDGES']
+#signal_type_list=['diffuse_global','noise']
+signal_type_list=['global_EDGES']
 #gsm_smooth_poly_order = 5
 #can be 5,6,or 7 for joint fitting
 poly_order = 7
