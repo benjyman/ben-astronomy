297d9b527a3b6829bc251424c70166449e414e28
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-06-08 11:16:29 +0800

    redo fig4a paper1 for color blind

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -2245,8 +2245,8 @@ def model_tsky_from_saved_data(freq_MHz_list,freq_MHz_index,lst_hrs,pol,signal_t
          ## plot X and real vis vs baseline length for fig4, paper 1
          
          plt.clf()
-         plt.scatter(baseline_length_array_lambda_sorted_cut,X_short_parallel_array_norm,label='Global response (unity sky)',color=color_dark_blue,marker='|',s=1)
-         plt.scatter(baseline_length_array_lambda_sorted_cut,real_vis_data_sorted_array_norm_scaled,label='Visibility amplitude (simulations)',color=color_orange_red,marker='_',s=1) #% real_or_simulated_string)
+         plt.scatter(baseline_length_array_lambda_sorted_cut,X_short_parallel_array_norm,label='Global response (unity sky)',color=color_dark_blue,marker='|',s=2)
+         plt.scatter(baseline_length_array_lambda_sorted_cut,real_vis_data_sorted_array_norm_scaled,label='Visibility amplitude (simulations)',color=color_orange_red,marker='_',s=2) #% real_or_simulated_string)
          #plt.plot(n_ants_array,expected_residuals,label='sqrt(n_arrays)',linestyle=':')
          map_title="Response to uniform sky vs baseline length data" 
          plt.xlabel("Baseline length (wavelengths)")
