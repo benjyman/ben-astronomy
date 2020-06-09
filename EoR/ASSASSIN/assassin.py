8bdf7348dec18b17603c8e22888ac90d5bc3d550
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-06-09 09:48:44 +0800

    redo fig10 paper1 for color blind

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -2604,7 +2604,7 @@ def model_tsky_from_saved_data(freq_MHz_list,freq_MHz_index,lst_hrs,pol,signal_t
    
       plt.clf()
       plt.plot(X_short_parallel_array_nonans, real_vis_data_sorted_array_flagged_K,label='%s data' % real_or_simulated_string,linestyle='None',marker='.')
-      plt.plot(X_short_parallel_array_nonans_nonans, fitted_values_K, 'r--.', label="OLS fit",linestyle='--',marker='None')
+      plt.plot(X_short_parallel_array_nonans_nonans, fitted_values_K,color=color_orange_red, label="OLS fit",linestyle='--',marker='None')
  
       #fig10 (and 5b and 6b for sims i think)
       map_title="Flagged data and fit" 
