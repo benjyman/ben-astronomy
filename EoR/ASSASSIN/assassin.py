010bd04a8d9fc606696093f6e8efaa075841e223
Benjamin <benjyman1024@gmail.com>
2020-06-25 15:59:26 +0800

    residual plots fix for Marcin feedback on paper

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -5175,7 +5175,7 @@ def plot_tsky_for_multiple_freqs(lst_hrs_list,freq_MHz_list,pol_list,signal_type
          print(residual_of_log_fit)
          
          plt.plot(freq_array_cut,residual_of_log_fit,label=label1)
-         plt.text(50, max_abs_residuals + y_offset, "rms=%2.1f K" % rms_of_residuals,{'color': colour})
+         plt.text(50, max_abs_residuals + y_offset, "rms=%1.2f K" % rms_of_residuals,{'color': colour})
          
          
          #include expected noise estimate:
