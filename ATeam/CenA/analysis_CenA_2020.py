193e7f1be3f56e9978ac5b57db3a921b12c20a5a
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-07-15 14:36:42 +0800

    cena update regridding rosat

--- ATeam/CenA/analysis_CenA_2020.py
+++ ATeam/CenA/analysis_CenA_2020.py
@@ -335,8 +335,9 @@ def regrid_concvol(image_1_name,image_2_name_list,target_bmaj_deg,target_bmin_de
       
    
    av_image_data = sum_image_data / count_image_data_sum
-   av_image_data = (av_image_data) + 0.00001
-   
+   av_image_data = np.nan_to_num(av_image_data) + 0.00001
+   print(av_image_data)
+   sys.exit()
    
    #write to fits:
    fits.writeto(mosaic_fits_name,av_image_data,clobber=True)
