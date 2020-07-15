0b44bf91a461900675a5e6a8b7ed2f97930513ed
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-07-15 13:20:09 +0800

    cena update regridding rosat

--- ATeam/CenA/analysis_CenA_2020.py
+++ ATeam/CenA/analysis_CenA_2020.py
@@ -319,11 +319,9 @@ def regrid_concvol(image_1_name,image_2_name_list,target_bmaj_deg,target_bmin_de
       
       overlap_area_data = sum_image_data + image_data_convol
       overlap_indices = np.argwhere(np.logical_not(np.isnan(overlap_area_data)))
-      
-      print(overlap_indices)
 
       sum_image_data += np.nan_to_num(image_data_convol)
-      #sum_image_data[overlap_indices] = sum_image_data[overlap_indices] / 2.
+      sum_image_data[overlap_indices] = overlap_area_data[overlap_indices] / 2.
       
       
    
