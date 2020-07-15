ab0275dfce7b05bcded42c91a08a5c730dacae28
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-07-15 16:31:13 +0800

    cena update regridding rosat

--- ATeam/CenA/analysis_CenA_2020.py
+++ ATeam/CenA/analysis_CenA_2020.py
@@ -364,8 +364,13 @@ def regrid_concvol(image_1_name,image_2_name_list,target_bmaj_deg,target_bmin_de
 
 #regridding x ray rosat from galacto:
 #regrid_concvol('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits','g000p00r1b120pm.fits',0.5,0.5,0,'CenA_rosat_north_to_mwa')
-cen_A_rosat_p10_list = ['932527p-p10.fits','932429p-p10.fits']
-regrid_concvol('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits',cen_A_rosat_p10_list,0.24,0.24,0,'rosat')
+#cen_A_rosat_p10_list = ['932527p-p10.fits','932429p-p10.fits']
+cen_A_rosat_p10_list = ['932428p-p10.fits','932429p-p10.fits','932430p-p10.fits','932526p-p10.fits','932527p-p10.fits','932528p-p10.fits','932623p-p10.fits','932624p-p10.fits','932625p-p10.fits']
+cen_A_rosat_p20_list = ['932428p-p20.fits','932429p-p20.fits','932430p-p20.fits','932526p-p20.fits','932527p-p20.fits','932528p-p20.fits','932623p-p20.fits','932624p-p20.fits','932625p-p20.fits']
+cen_A_rosat_p30_list = ['932428p-p30.fits','932429p-p30.fits','932430p-p30.fits','932526p-p30.fits','932527p-p30.fits','932528p-p30.fits','932623p-p30.fits','932624p-p30.fits','932625p-p30.fits']
+
+
+regrid_concvol('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits',cen_A_rosat_p10_list,0.24,0.24,0,'rosat_low')
 sys.exit()
 
 #image_name = "CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits"  
