c79e1b557035e4aa63abadc475713e50e10da99d
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-07-15 11:44:05 +0800

    cena update regridding rosat

--- ATeam/CenA/analysis_CenA_2020.py
+++ ATeam/CenA/analysis_CenA_2020.py
@@ -267,7 +267,7 @@ def regrid_concvol(image_1_name,image_2_name_list,target_bmaj_deg,target_bmin_de
       except:
          image_header['CTYPE3'] = 'FREQ'
          image_header['CRVAL3'] = 184955000.
-         image_header['CRPIX3'] = 1.
+         image_header['CRPIX3'] = 1
          image_header['CDELT3'] = 30720000.
          image_header['CUNIT3'] = 'Hz'
       #resolution of PSPC on XRT on Rosat about 20''
