0eddc3d4b4bb8d86e8732ed7de007951f1ff6c64
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-10-12 12:53:51 +0800

    do WODEN stuff in assassin.py

--- EoR/ASSASSIN/assassin.py
+++ EoR/ASSASSIN/assassin.py
@@ -11975,7 +11975,46 @@ def plot_internal_noise_coupling(frequency_MHz_array,mnm_odd_filename,antenna_po
       plt.gcf()
       plt.savefig(plot_filename)
       print("save %s" % plot_filename)   
+          
+def write_woden_sourcelists(hpx_fits_filename,nside):
+   name_base = hpx_fits_filename.split(".fits")[0]          
+   data = hp.read_map(hpx_fits_filename,nest=False)
+   pix_inds = arange(hp.nside2npix(nside))
+   l, b = hp.pix2ang(nside,pix_inds,lonlat=True)
+   gal_coords = SkyCoord(l*u.deg, b*u.deg, frame='galactic')
+   ra = gal_coords.icrs.ra.value
+   dec = gal_coords.icrs.dec.value
+   fluxes = data*hp.nside2pixarea(nside,degrees=False)*1e+6
+   fig = plt.figure(figsize=(10,10))
+   hp.mollview(log10(data), sub=(2,1,1), fig=fig,title='Galactic')
+   hp.mollview(log10(fluxes), sub=(2,1,2), fig=fig,title='Equatorial')
+   fig.savefig('%s_woden_map.png' % name_base, bbox_inches='tight')
+   plt.close()
+   source_ind = 0
+   with open('%s_woden.xt' % name_base,'w') as outfile:
+       for ind,flux in enumerate(fluxes):
+           if source_ind == 0:
+               outfile.write('SOURCE pygsm P %d G 0 S 0 0\n' %len(fluxes))
+           outfile.write('COMPONENT POINT %.7f %.6f\n' %(ra[ind]/15.0,dec[ind]))
+           outfile.write('LINEAR 150e+6 %.10f 0 0 0 0.0\n' %flux)
+           outfile.write('ENDCOMPONENT\n')
+           source_ind += 1
+       outfile.write('ENDSOURCE')
+
+def write_woden_skymodels(centre_chans_number_list,nside):
+    for centre_chan in centre_chans_number_list:
+       for band in range(0,24):
+          name_base = "gsm_model_centre_chan_%03d_band_%02d_freq_%0.3f_MHz_hpx"
+          
+
+          
+def write_woden_sims_sbatch_file(centre_chan_number_list):
+   print("for centre chans:") 
+   print(centre_chan_number_list)
    
+   
+  
+
 #internal_noise_matrix_filename = "/md0/EoR/ASSASSIN/noise_coupling/mnm_even_eda2.npy"
 #antenna_positions_filename = "/md0/code/git/ben-astronomy/EoR/ASSASSIN/eda2_antenna_order_daniel_NEU.txt"
 #frequency_MHz_array_mnm = (np.arange(0,218) * 1.28 ) + 50
