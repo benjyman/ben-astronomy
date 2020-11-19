f6b35d1146792b979ff0299ec70b7f7c7838112c
Benjamin <benjyman1024@gmail.com>
2020-11-19 17:17:44 +0800

    changes to be consistent with Maxs Cen A calcs - all good

--- ATeam/CenA/analysis_CenA_2020.py
+++ ATeam/CenA/analysis_CenA_2020.py
@@ -13,9 +13,11 @@ c = 299792458.
 M_bh_cena = 1.0e8 * M_solar
 pc_m = 3.0857e16 
 yr_sec = 3.1557600e7
-mp = 1.6726219e-27
+mp = 1.6726219e-27    #kg
 kb = 1.38064852e-23
 eV_J = 1.602176634e-19
+erg_J = 1.0e-7
+dyn_cm2_Pa = 0.1
 
 
 def get_scaling_factor_from_core(image_name,freq_MHz,alpha):
@@ -235,24 +237,25 @@ def spectral_index_map(image_1_name,image_2_name,freq_MHz_low,freq_MHz_high,outp
    print(cmd)
    os.system(cmd)
    
+   
    ##############
    #OPTIONAL FITS output
    #output the regrid convol
-   regrid_convol_fitsname = "rosat_diffuse_north_regrid_convol_mwa_2deg.fits"
-   cmd = "rm -rf %s" % (regrid_convol_fitsname)
-   print(cmd)
-   os.system(cmd)  
-
-   cmd = "fits in=%s out=%s op=xyout" % (im_name_high_regrid_convol,regrid_convol_fitsname)
-   print(cmd)
-   os.system(cmd) 
+   #regrid_convol_fitsname = "rosat_diffuse_north_regrid_convol_mwa_2deg.fits"
+   #cmd = "rm -rf %s" % (regrid_convol_fitsname)
+   #print(cmd)
+   #os.system(cmd)  
+   #
+   #cmd = "fits in=%s out=%s op=xyout" % (im_name_high_regrid_convol,regrid_convol_fitsname)
+   #print(cmd)
+   #os.system(cmd) 
  
    #make the spec index map
    cmd = "rm -rf %s %s" % (spec_index_im_name,spec_index_fits_name)
    print(cmd)
    os.system(cmd)
   
-   cmd = "maths exp=log\(%s/%s\)/log\(%0.2f/%0.2f\) mask=%s>%0.5f  out=%s" % (im_name_high_regrid_convol,im_name_low,freq_MHz_high,freq_MHz_low,im_name_high_regrid_convol,mask_level,spec_index_im_name)
+   cmd = "maths exp=log\(%s/%s\)/log\(%0.2f/%0.2f\) mask=%s.gt.%0.5f  out=%s" % (im_name_high_regrid_convol,im_name_low,freq_MHz_high,freq_MHz_low,im_name_high_regrid_convol,mask_level,spec_index_im_name)
    print(cmd)
    os.system(cmd) 
    
@@ -387,13 +390,15 @@ def regrid_concvol(image_1_name,image_2_name_list,target_bmaj_deg,target_bmin_de
    #os.system(cmd)
 
 def calculate_outflow_properties():
+#G17 scale defs
    r_s_m = 2. * G * M_bh_cena / c**2
    r_s_pc = r_s_m / pc_m
-   print("r_s_pc is %0.9f" % r_s_pc)
-   print("5 * r_s_pc is %0.9f pc" % (r_s_pc*5.) )
-   print("100 * r_s_pc is %0.9f pc" % (r_s_pc*100) )
-   print("1e9 * r_s_pc is %0.9f pc" % (r_s_pc*1e9) )
-   print("1e10 * r_s_pc = r_vir is %0.9f pc" % (r_s_pc*1e10) )
+   print("r_s_pc is %0.9f = %0.3f kpc" % (r_s_pc,r_s_pc/1000.0))
+   print("5 * r_s_pc is %0.9f pc = %0.3f kpc" % (r_s_pc*5.,r_s_pc*5./1000.) )
+   print("Micro: 1000 * r_s_pc is %0.9f pc = %0.3f kpc" % (r_s_pc*1000.,r_s_pc*1000./1000.) )
+   print("Meso: 1e6 * r_s_pc is %0.9f pc = %0.3f kpc" % (r_s_pc*1.e6,r_s_pc*1e6/1000.) )
+   print("Macro: 1e9 * r_s_pc is %0.9f pc = %0.3f kpc" % (r_s_pc*1.e9,r_s_pc*1e9/1000.) )
+   print("r_vir 1e10 * r_s_pc = r_vir is %0.9f pc = %0.3f kpc" % (r_s_pc*1.e10,r_s_pc*1.e10/1000.) )
    
    #israel et al 2017 inner 500 pc outflow
    vel_km_s = 60.
@@ -411,7 +416,7 @@ def calculate_outflow_properties():
    
    print("Israel et al 2017 inner 500pc ouflow time is %0.5f Myr" % outflow_time_Myr)
    print("mass outflow rate %0.5f solar mass/yr " % mass_outflow_rate_solarmass_yr)
-   
+   print("actual velocity %0.5f km/s " % (actual_velocity_m_s/1000.))
    
    #krol et al 2020 kpc cold outflow
    line_of_sight_theta_deg = 45.
@@ -465,25 +470,254 @@ def calculate_outflow_properties():
    gamma = 5./3.
    mu = 0.62 #is the average atomic weight for a fully ionized plasma with 25% He in mass
    Tx_ev = 0.5 * 1000.
-   #Tx_J = eV_J * Tx_ev
+   Tx_J = (eV_J * Tx_ev) / kb
    #adiabatic sound speed
-   cs_ms = (gamma * kb * Tx_ev / (mu * mp))**(0.5)
+   cs_ms = (gamma * kb * Tx_J / (mu * mp))**(0.5)
    
-   constant = ((gamma * kb) / (mu * mp))**(0.5)
-   print(constant)
+   #constant = ((gamma * kb) / (mu * mp))**(0.5)
+   #print(constant)
    #cs_ms = 1.5e4 * (Tx_ev)**(0.5)
-   #cs_kms = cs_ms / 1000.
-   #print(cs_kms)
-   #print("cs is %0.5f km per sec" % cs_kms)
+   #the factor 100 is there I think because G17 is wrong in its 10**4 value in sec 3.1 eqn
+   cs_kms = cs_ms / 1000. 
+   print("cs is %E km per sec" % cs_kms)
+   
+   #G17 stuff:
+   #from MWA image: 
+   r_thermalisation_kpc = 75.
+   #r_thermalisation_kpc = 55. * (T_x_7p4)**(3./2.)  (EQN 26)
+   #T_x_7p4 = (r_thermalisation_kpc/55.0)**(2./3.)
+   #print("T_x_7p4 is %E keV" % T_x_7p4)
+   
+   #EQN 18:
+   #v_out_km_s = 1.4e4 * T_x_7p4**(0.5)
+   #print("inner vel v_out_km_s is %E " % v_out_km_s)
+   
+   #(EQN 23 )
+   #entrainment_factor_hot = 40. * 1.**(2./3.) / T_x_7p4
+   #print("entrainment_factor_hot is %E " % entrainment_factor_hot)
+   
+   #outer vel (EQN 21)
+   #V_OUT_km_s = entrainment_factor_hot**(-0.5) * v_out_km_s
+   #print("macro vel V_OUT_km_s is %E " % V_OUT_km_s)
+   
+   
+   #EQN23 
+   
+   #sigma_steradians = 4.*np.pi
+   #for plasma density:  Look at (from Max) OSullivan et al 2003 or Bogdan et al. 2010 for Chandra, plus Borkar 2020 for inner 180 pc
+   #from borkar 2020 / kraft 2003 - use n(r) = n0 (r/r0)**-1.5, with n0 = 3.7e-2 cm-3, and r0=10 pc
+   #hot_plasma_density = 0.4 * mp * 1.0e3               # 1.e-25 #
+   #print("hot_plasma_density is %E" % hot_plasma_density)
+   #ref_radius_kpc = 2.7
+   #constant_eqn_23 = (sigma_steradians/(4.*np.pi)) * (hot_plasma_density/1.0e-25)* 40.
+   #print("constant_23 = %0.5f" % constant_eqn_23)
+   
+   #neff NML
+   #radius = 30.
+   #width = 24.
+   #sigma_steradians = 2. * 2. * np.pi * (1. - np.cos((math.atan(width/radius) / 2)))  # see wikepedia page on solid angle! ...extra factor of 2 because bipolar
+   #print(sigma_steradians)
+   #frac_sphere = sigma_steradians / (4.*np.pi)
+   #print(frac_sphere)
+   #constant_eqn_23 = (sigma_steradians/(4.*np.pi)) * (hot_plasma_density/1.0e-25)* 40.
+   #print("constant_23 neff = %0.5f" % constant_eqn_23)
+   
+   
+   #Gaspari eqn 26 (assumes omega is 4pi)
+   #try using T_x_kraft_2009
+   #sigma = 4.*np.pi
+   radius = 63. #adj
+   width = 90.  #opp = width/2
+   print("########from MWA image estimate that the outflow has spread to a width of %s kpc at a distance of %s kpc from the core" % (radius,width))
+   #tan(theta) = opp/adj
+   theta = math.atan((width/2.)/radius)
+   print("theta is %0.3f rad" % theta) 
+   theta_deg = theta / np.pi * 180.
+   print("#####theta_deg is %0.3f deg" % theta_deg) 
+   opening_angle_deg = 2.*theta_deg
+   sigma = 2. * np.pi * (1. - np.cos(theta))  # see wikepedia page on solid angle!
+   print("#####opening solid angle sigma is %0.3f sr" % sigma)
+   #T_x_kraft_2009 = 0.35 #keV
+   #T_x_7p4 = T_x_kraft_2009 / 2.2
+   const_eqn_26 = 55. / (sigma*2 / (4.*np.pi)) #kpc
+   print("######const_eqn_26 is %E" % const_eqn_26)
+   #r_th = const_eqn_26 * T_x_7p4**(3./2.)
+   #print("r_th is %E kpc" % r_th)
+   
+   
+   #Reversing this so we have r_th measured from radio:
+   r_th = 75.
+   const_eqn_26 = 55. / (sigma / (4.*np.pi)) #kpc
+   T_x_7p4 = (r_th / const_eqn_26)**(2./3.)
+   print("######T_x_7p4 is %E keV" % T_x_7p4)
+   T_x_unscaled = T_x_7p4 * 2.2
+   print("######T_x_unscaled is %E keV" % T_x_unscaled)
+
+   #MWA south (arcmin):
+   #radius = 63.
+   #width = 90.
+   #sigma_steradians = 2. * 2. * np.pi * (1. - np.cos((math.atan(width/radius) / 2)))  # see wikepedia page on solid angle! ...extra factor of 2 because bipolar
+   ##sigma_steradians = 4.*np.pi
+   #print(sigma_steradians)
+   #sigma_degrees = sigma_steradians / np.pi * 180.
+   #print("opening angle is south is %0.2f degrees" % sigma_degrees)
+   #frac_sphere = sigma_steradians / (4.*np.pi)
+   #print(frac_sphere)
+   
+   constant_eqn_23 = 40. * (sigma/(4.*np.pi))**(2./3.) #* (hot_plasma_density/1.0e-25) * (ref_radius_kpc/1.)
+   print("########constant_23 MWA south = %0.5f" % constant_eqn_23)
+   
+   ##eqn 26
+   #const_eqn_26 = 55. * (sigma_steradians / (4.*np.pi))**(2./3.) * (hot_plasma_density/1.0e-25)**(2./3.) * (ref_radius_kpc/1.)**(2./3.)
+   #print("const_eqn_26 = %0.5f" % const_eqn_26)
+   
+   ##r_thermalisation_kpc = const_eqn_26 * (T_x_7p4)**(3./2.)  (EQN 26)
+   #T_x_7p4 = (r_thermalisation_kpc/const_eqn_26)**(2./3.)
+   #if we take the kraft et al value of Tx:
+   ##T_x_7p4 = 0.35
+   ##print("T_x_7p4 is %E keV" % T_x_7p4)
+   
+   #Tx_unscaled = T_x_7p4 * 2.2
+   #print("Tx_unscaled is %E keV" % Tx_unscaled)
+   
+   
+   #EQN 23
+   entrainment_factor_hot = constant_eqn_23 * 1.**(2./3.) / T_x_7p4
+   print("########entrainment_factor_hot is %E " % entrainment_factor_hot)
+   
+   #EQN 18: (inner outflow)
+   v_out_km_s = 1.4e4 * T_x_7p4**(0.5)
+   print("#########inner vel v_out_km_s is %E " % v_out_km_s)
+   
+   #outer outflow vel (EQN 21)
+   V_OUT_km_s = entrainment_factor_hot**(-0.5) * v_out_km_s
+   print("#########macro vel V_OUT_km_s is %E " % V_OUT_km_s) 
+   
+   #try equation 20 G17
+   #ro_nought = 1. #kpc
+   #M_OUT = sigma * ro_nought * r_nought * r_th * V_OUT_km_s
+   
+   
+   ##EQN 20 outer mass outflow 
+   #M_OUT = (sigma_steradians / (4.*np.pi)) *  (hot_plasma_density/1.0e-25) * (ref_radius_kpc/1.) * V_OUT_km_s
+   #print("Don't know units! macro mass outflow rate M_OUT is %E " % M_OUT)
+   ##EQN 19 outer mass outflow 
+   
+   #ENQN 7 Macro mass inflow rate M_cool (also look at sun2012)
+   #T_x_7p4 = 0.8
+   M_cool = 1.1 * T_x_7p4**2
+   print("#########Macro mass inflow rate M_cool is %E solar mass / yr" % M_cool) 
+   
+   #micro outflow rate is approximately equal to the inflow rate (only a few percent of the mass is actually able to fall into the BH)
+   #EQN 16
+   m_out = M_cool
+   print("#########micro mass outflow rate m_out is %E solar mass / year (roughly same as M_cool)" % m_out) 
+   
+   M_OUT = m_out * entrainment_factor_hot 
+   print("#########Macro mass outflow rate (hot) m_out is %E solar mass / year" % M_OUT) 
+   
+   #The final link! G17 Eqn 12 - the micro mass inflow rate
+   M_dot_in_micro = 0.03 * M_cool * T_x_7p4 
+   print("#########micro mass inflow rate M_dot_in_micro is %E solar mass / year" % M_dot_in_micro) 
+   
+   #check if power of wind can create the x-ray clouds consistent with Kaft et al 2009:
+   #G17 eqn 15:
+   #M_OUT = 0.5
+   #V_OUT_km_s = 2000.
+   M_OUT_kg_s = M_OUT * M_solar / yr_sec
+   print("M_OUT_kg_s is %E" % M_OUT_kg_s)
+   power_out_macro_W = 0.5 * M_OUT_kg_s * (V_OUT_km_s * 1000.) ** 2.0
+   print("power_out_macro_W is %E" % power_out_macro_W)
+   power_out_macro_erg_s = power_out_macro_W / erg_J
+   print("power_out_macro_erg_s is %E" % power_out_macro_erg_s)
+   
+   #NML power from kraft et al 2009:
+   kraft_nml_radius_m = 35. * 1000. * pc_m
+   kraft_nml_volume_m3 = 4./3. * np.pi * kraft_nml_radius_m**3
+   kraft_nml_pressure_dyn_cm2 = 8.5e-13
+   kraft_nml_pressure_N_m2 = kraft_nml_pressure_dyn_cm2 * 0.1
+   E_mech_J = 4. * kraft_nml_pressure_N_m2 * kraft_nml_volume_m3
+   E_mech_erg = E_mech_J / erg_J
+   print("Kraft nml E_mech_erg is %E erg" % E_mech_erg)
+   time_for_power_out_macro_s = E_mech_erg / power_out_macro_erg_s
+   time_for_power_out_macro_Myr = time_for_power_out_macro_s / yr_sec / 1.e6
+   print("time_for_power_out_macro_Myr is %E" % time_for_power_out_macro_Myr)
+   
+   
+   
+   ##Israel hot ionised  outflow
+   #this number density is just for e-, dont know hot gas density at 1kpc - maybe could use krol at 2.7?
+   #number_density_cm3 = 10.
+   #density_g_cm3 = number_density_cm3 * mp #* mu
+   #print("density_g_cm3 is %E" % density_g_cm3)
+   #constant_eqn_23 = (sigma_steradians/(4.*np.pi)) * (density_g_cm3/1.0e-25)* 40.
+   #print("constant_23 MWA south and israel CND density = %0.5f" % constant_eqn_23)
+   
+   
+   
+   #stuff from Tombesi 2014 (table 1)
+   CenA_v_out_c = 0.005
+   CenA_v_out_kms = CenA_v_out_c * c / 1000.
+   s3C445_v_out_kms = 0.034 * c / 1000.
+   #print("CenA UFO velocity is %0.5f km/sec for %0.5f c" % (CenA_v_out_kms,CenA_v_out_c))
+   #print("compare to 3C445 UFO velocity is %0.5f km/sec" % s3C445_v_out_kms)
+   
+   #radio jet
+   v_in_c = .1
+   v_kms = v_in_c * c / 1000.
+   #print("CenA UFO velocity is %0.5f km/sec for %0.5f c" % (v_kms,v_in_c))
+   v_in_c = .45
+   v_kms = v_in_c * c / 1000.
+   #print("CenA UFO velocity is %0.5f km/sec for %0.5f c" % (v_kms,v_in_c))
+   
+   #temperature conversions
+   temp_kT_k_eV = 0.16
+   temp_kT_k_J = 1000. * eV_J * temp_kT_k_eV
+   temp_T_K = temp_kT_k_J / kb
+   #print("temp_kT %0.5f keV = temp_K %4E K " % (temp_kT_k_eV,temp_T_K))
+   
+   #HI stuff
+   total_mass = 1.5e8 * M_solar
+   
+   #Eilek density: 
+   B_field_micro = 1
+   n_p = 3e-7 * B_field_micro**2
+   #print("np frowm Eilek with 1 micro Gauss field is %E" % n_p)
+   
+   
+   #Neff et al 2015b stuff:
+   thermal_pressure_ism_neff_dyn_cm2 = 8.5e-13
+   thermal_pressure_ism_neff_Pa = thermal_pressure_ism_neff_dyn_cm2 * 0.1
+   power_1_neff_erg_s = 1.0e42
+   power_1_neff_J_s = power_1_neff_erg_s * 1e-7
+   #velocity_wind_1_neff_ms = 2000.0 * 1000.
+   
+   power_2_neff_erg_s = 1.0e43
+   power_2_neff_J_s = power_2_neff_erg_s * erg_J
+   #velocity_wind_2_neff_ms = 6300.0 * 1000.
+   
+   radius_shock_neff_1_m = np.sqrt(power_1_neff_J_s / (4. * np.pi * thermal_pressure_ism_neff_Pa))
+   radius_shock_neff_1_kpc = (radius_shock_neff_1_m / pc_m) / 1000.
+   #print("radius_shock_neff_1_kpc is %E kpc" % radius_shock_neff_1_kpc)
+
+   radius_shock_neff_2_m = np.sqrt(power_2_neff_J_s / (4. * np.pi * thermal_pressure_ism_neff_Pa))
+   radius_shock_neff_2_kpc = (radius_shock_neff_2_m / pc_m) / 1000.
+   #print("radius_shock_neff_2_kpc is %E kpc" % radius_shock_neff_2_kpc)
+   
    
    
 calculate_outflow_properties()
 sys.exit()
 
+
+#mask_level=0.1
 #spectral index ASKAP MWA:
 #spectral_index_map('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits','CenA_i.fits',185,1400,'CenA_185_1400_MHz',0.3)
 #spectral_index_map('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits','image_cenauc13_image_tt0.fits',185,1400,'CenA_uc13_185_1400_MHz',0.3)
-#sys.exit()
+#spectral_index_map("CenA_2015_2018_joint_145_robust0_image_pb_8_ims_0000_08_weighted.fits","CenA_2015_2018_joint_145_robust0_image_pb_8_ims_0009_08_weighted.fits",171.135,198.755,'CenA_171_198_MHz',mask_level)
+
+
+sys.exit()
 
 #regridding x ray rosat from galacto:
 #regrid_concvol('CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits','g000p00r1b120pm.fits',0.5,0.5,0,'CenA_rosat_north_to_mwa')
@@ -530,14 +764,14 @@ template_imagename = 'CenA_optical_template-image.fits'
 #mike sidonio:
 #input_name_list = ['CenA_Ha_1050min_median.fits','CenA_lum_1470min_median_grad.fits']
 #input_name_list = ['CenA_Ha_1050min_median_solved.fits','CenA_Lum_1470min_median_grad_solved.fits']
-input_name_list = ['3_Separate_HII_regions_from_Ha.fits']
+input_name_list = ['CenA_2015_2018_joint_145_robust0_image_pb_8_ims_08_weighted.fits']
 #i know this one works:
 #input_name_list = ['CenA_WCS.fits']
 #input_name_list = ['CenA_WCS_Ha.fits']
 for input_name in input_name_list:
-   edhead_name = input_name.split('.fits')[0]+'_edhead.fits'
-   edit_optical_header(input_name,edhead_name)
-   regrid_optical(template_imagename,edhead_name,smooth=2)
+   #edhead_name = input_name.split('.fits')[0]+'_edhead.fits'
+   #edit_optical_header(input_name,edhead_name)
+   regrid_optical(template_imagename,input_name)    #edhead_name,smooth=2)
    #try no edhead for mikes:
    #regrid_optical(template_imagename,input_name)
    
