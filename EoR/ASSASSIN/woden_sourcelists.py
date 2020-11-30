83f81510c6e11e87e938d68e0882660f5793519e
Benjamin McKinley <ben.mckinley@unimelb.edu.au>
2020-11-30 11:21:38 +0800

    added timestring stuff to woden sourcelists

--- EoR/ASSASSIN/woden_sourcelists.py
+++ EoR/ASSASSIN/woden_sourcelists.py
@@ -11,6 +11,7 @@ import numpy as np
 from astropy import units as u
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
+from assassin import calc_beam_values
 
 c = 299792458.
 k = 1.38065e-23
