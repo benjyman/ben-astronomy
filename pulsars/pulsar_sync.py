a2420d17e067d8296082f84152c3126a52f96ac0
Benjamin <benjyman1024@gmail.com>
2021-01-06 13:30:13 +0800

    download catalogue

--- pulsars/pulsar_sync.py
+++ pulsars/pulsar_sync.py
@@ -1,1 +1,10 @@
 #check pulse pulse period ratios
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from psrqpy import QueryATNF
+
+#download the catalogue
+query = QueryATNF()
+table = query.table
