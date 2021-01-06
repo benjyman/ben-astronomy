7f7fb08aef3e1cc1d432990099aa906f1dc0bc61
Benjamin <benjyman1024@gmail.com>
2021-01-06 14:17:26 +0800

    download catalogue

--- pulsars/pulsar_sync.py
+++ pulsars/pulsar_sync.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #check pulse pulse period ratios
 import matplotlib
 matplotlib.use('Agg')
@@ -8,4 +8,28 @@ from psrqpy import QueryATNF
 
 #download the catalogue
 query = QueryATNF()
-table = query.table
+query.save('atnfquery.pkl')
+
+numstring = 'Version {} of the ATNF catalogue contains {} pulsars'
+print(numstring.format(query.get_version, query.num_pulsars))
+
+#pandas
+df = query.pandas
+
+num_pulsars_used = 20
+pulsars_used = df.iloc[0:num_pulsars_used,:]
+
+print(pulsars_used)
+
+print(df.columns)
+
+
+#calculate the ratio of the pulse periods and the distance separating each pulsar pair
+for pulsar1_index in range(0,num_pulsars_used):
+   pulsar1_name = df.loc[pulsar1_index,'PSRJ']
+   pulsar1_period = 
+   for pulsar2_index in range(0,num_pulsars_used):
+      #only uniques baselines and dont include autos
+      if (pulsar2_index > pulsar1_index):
+         pulsar2_name = df.loc[pulsar2_index,'PSRJ']
+         print("pulsar1 name %s, pulsar2 name %s" % (pulsar1_name,pulsar2_name))   
